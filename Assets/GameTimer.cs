20908f5e59fff37370cfb78c46988e211148036a
이동현 <dlehdgus0413@naver.com>
2020-01-27 14:11:47 +0900

    첫 커밋

---
+++ Assets/GameTimer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameTimer : MonoBehaviour {
+
+
+    public Text timerText;
+    private float _gameTimer;
+    private int minutsTime;
+
+	void Start () {
+        _gameTimer = 0f;
+	}
+	
+	void Update () {
+        timerText.text = string.Format("{0} : {1}", minutsTime, (int)_gameTimer);
+        if (minutsTime >= 5)
+        {
+            UIManager.Instance.GameOver();
+            return;
+        }
+
+        _gameTimer += Time.deltaTime;
+
+        if(_gameTimer >= 60f)
+        {
+            minutsTime++;
+            _gameTimer = 0f;
+        }
+    }
+}
---
+++ Assets/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour {
+
+    public static UIManager Instance;
+
+    public GameObject gameover;
+
+    private void Awake()
+    {
+        if(Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
+
+    public void GameOver()
+    {
+        gameover.SetActive(true);
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
